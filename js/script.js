e201a8768fbcfda24248fc1491cf2a80195998c2
paulinaboron <paulinaboron04@gmail.com>
2022-08-21 01:14:10 +0200

    banner & navigation - start

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function () {
+
+    
+    // Opening animation
+    let title = $("#title")
+    title.addClass('red')
+    console.log('ok');
+
+
+    // Showing navigation with scroll up
+    const nav = $("#nav")
+    let lastScroll = 0
+
+    window.addEventListener('scroll', () => {
+        let currentScroll = window.pageYOffset
+
+        if (currentScroll <= 0) {
+            nav.removeClass('scrollUp')
+        }
+
+        if (currentScroll > lastScroll && !nav.hasClass('scrollDown')) {
+            nav.removeClass('scrollUp')
+            nav.addClass('scrollDown')
+        }
+
+        if (currentScroll < lastScroll && nav.hasClass('scrollDown')) {
+            nav.removeClass('scrollDown')
+            nav.addClass('scrollUp')
+        }
+
+        lastScroll = currentScroll
+    })
+
+
+
+})
